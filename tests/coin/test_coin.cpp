bc6832b0958a6c640bd120fccd5a44828fdd90d8
diyooor <diyorsattarov7@gmail.com>
2023-12-14 12:21:42 -0600

    Added  multiple flips distribution tests

--- tests/coin/test_coin.cpp
+++ tests/coin/test_coin.cpp
@@ -3,10 +3,34 @@
 
 class CoinTest : public ::testing::Test {
 protected:
+    Coin coin;
     void SetUp() override {}
     void TearDown() override {}
 };
 
 TEST_F(CoinTest, FirstCoinTest) {
-    ASSERT_EQ(1, 1);
+    ASSERT_EQ(coin.getState(), CoinState::Heads);
+}
+
+TEST_F(CoinTest, MultipleFlipsDistribution) {
+    float headsAverage = 0;
+    float tailsAverage = 0;
+    for (int j = 0; j < 10; ++j) {
+        int heads = 0;
+        int tails = 0;
+        const int numFlips = 1000;
+
+        for (int i = 0; i < numFlips; ++i) {
+            if (coin.flip() == CoinState::Heads) {
+                ++heads;
+            } else {
+                ++tails;
+            }
+        }
+        Utilities::console_logger()->info("Heads: {} Tails: {}", heads, tails);
+        ASSERT_NEAR(heads, tails, numFlips * 0.1); // Allowing 10% deviation
+        headsAverage += heads;
+        tailsAverage += tails;
+    }
+    Utilities::console_logger()->info("headsAverage: {} tailsAverage: {}", headsAverage/100, tailsAverage/100);
 }
